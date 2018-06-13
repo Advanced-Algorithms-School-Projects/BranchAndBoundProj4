d766fa71c6405de977a85f61b9ebb94e33dedc98
cassiels636 <35430596+cassiels636@users.noreply.github.com>
2018-06-13 17:27:31 -0400

    More stuff

--- p4.cpp
+++ p4.cpp
@@ -15,7 +15,10 @@ using namespace std;
 #include "d_matrix.h"
 #include "knapsack.h"
 
-void branchAndBound(knapsack &k, int);
+void branchAndBound(knapsack &k, int t);
+void branchRecursive(knapsack &bestSol, stack<knapsack> &branches);
+boolean isFathomed(knapsack &k, knapsack &bestSol);
+
 
 int main()
 {
@@ -62,5 +65,51 @@ int main()
 }
 
 void branchAndBound(knapsack &k, int t) {
+	knapsack bestSol = knapsack(k);
+	stack<knapsack> branches;
 
+	branches.push(k);
+	branchRecursive(k, bestSol, branches);
+
+}
+
+void branchRecursive(knapsack &bestSol, stack<knapsack> &branches) {
+	for (int 0; i <= 1; i++) {
+		knapsack temp = knapsack(branches.top());
+		temp.setLevel = branches.top().getLevel() + 1;
+		if (i == 1) {
+			temp.select(temp.getLevel());
+		}
+		branches.push(temp);
+		if (!isFathomed(branches.top(), bestSol)) {
+			branchRecursive(bestSol, branches);
+		}
+		else {
+			branches.pop();
+		}
+	}
+	branches.pop();
+	return;
+}
+
+boolean isFathomed(knapsack &k, knapsack &bestSol) {
+	if (k.getCost() > k.getCostLimit()) {
+		return true;
+	}
+	else if (k.getCost() == k.getCostLimit()) {
+		if (k.getValue() > bestSol.getValue()) {
+			bestSol = knapsack(k);
+		}
+		return true;
+	}
+	else if (k.bound() <= bestSol.getValue()) {
+		return true;
+	}
+	else if (level == k.getNumObjects()) {
+		if (k.getValue() > bestSol.getValue()) {
+			bestSol = knapsack(k);
+		}
+		return true;
+	}
+	return false;
 }
